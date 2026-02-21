f50f2c9ce81b99fa48a7e9095f5fb99d9ae97b32
Wilbert Caba <wilbertcaba@gmail.com>
2026-02-21 00:51:54 -0500

    feat: add migration for new pages and blocks schema with enums and tables

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260216_210157 from './20260216_210157';
+
+export const migrations = [
+  {
+    up: migration_20260216_210157.up,
+    down: migration_20260216_210157.down,
+    name: '20260216_210157'
+  },
+];
